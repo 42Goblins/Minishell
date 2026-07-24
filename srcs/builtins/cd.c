b4c2fe8532c802ee106fdf96e95dd5abbced4ca7
deltafraktal <dougea71@gmail.com>
2026-07-25 01:23:51 +0200

    Revert "feat: cd: v1 untested, 1 too many function"
    This reverts commit d741dc27f35226dd084d3c0c66ebbec20bce65aa.

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,25 +6,11 @@
 /*   By: dgeara <dgeara@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 17:34:49 by dgeara            #+#    #+#             */
-/*   Updated: 2026/07/24 03:10:37 by dgeara           ###   ########.fr       */
+/*   Updated: 2026/07/23 04:30:36 by dgeara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	set_env_value(t_env *env, const char *key, const char *value)
-{
-	while (env)
-	{
-		if (ft_strcmp(env->key, key) == 0)
-		{
-			free(env->value);
-			env->value = ft_strdup(value);
-			return ;
-		}
-		env = env->next;
-	}
-}
 
 void	update_env(t_env *env)
 {
@@ -42,18 +28,7 @@ void	update_env(t_env *env)
 		set_env_value(env, "OLDPWD", oldpwd);
 }
 
-char	*get_env_value(t_env *env, const char *key)
-{
-	while (env)
-	{
-		if (ft_strcmp(env->key, key) == 0)
-			return (env->value);
-		env = env->next;
-	}
-	return (NULL);
-}
-
-void	go_to_oldpwd(t_env *env)
+void	go_to_prev_dir(t_env *env)
 {
 	char	*oldpwd;
 
@@ -83,11 +58,23 @@ void	exec_cd(t_shell *shell, char **cmd)
 	if (!cmd[1] || (ft_strncmp(cmd[1], "~", 2) == 0))
 		return (go_to_home_dir(shell->env));
 	if (strncmp(cmd[1], "-", 2) == 0)
-		return (go_to_oldpwd(shell->env));
+		return (go_to_prev_dir(shell->env));
 	if (!chdir(cmd[1]))
 	{
 		ft_putstr_fd("minishell: cd:", 2);
 		perror(cmd[1]);
 	}
 	update_env(shell->env);
+	//update env (getcwd)
+}
+
+char	*get_env_value(t_env *env, const char *key)
+{
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+			return (env->value);
+		env = env->next;
+	}
+	return (NULL);
 }
