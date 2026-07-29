8b3312c4ee57cd990e15864c618e9b8ddc14016f
Dounia Geara <dougea71@gmail.com>
2026-07-29 18:46:30 +0200

    chore: #3 unset: recleaned funcitons after tests

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,16 +6,16 @@
 /*   By: dgeara <dgeara@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 03:47:59 by dgeara            #+#    #+#             */
-/*   Updated: 2026/07/25 04:12:04 by dgeara           ###   ########.fr       */
+/*   Updated: 2026/07/29 18:45:19 by dgeara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exec_pwd()
+int	exec_pwd(void)
 {
 	char	*pwd;
-	
+
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		return (perror("getcwd :"), 1);
