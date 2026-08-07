42b22946546fb7e302888a12360f4f08897d21da
Dounia Geara <dougea71@gmail.com>
2026-08-07 03:06:42 +0200

    début petard

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dgeara <dgeara@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 17:34:41 by dgeara            #+#    #+#             */
-/*   Updated: 2026/08/04 05:22:15 by dgeara           ###   ########.fr       */
+/*   Updated: 2026/08/07 03:06:06 by dgeara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void set_shell(t_shell *shell, t_cmd *cmd, char **env)
 }
 void set_cmd(t_cmd *cmd)
 {
-	static char *cd_args[] = {"export", "PATATE", NULL};
+	static char *cd_args[] = {"ls", NULL};
 	cmd->cmd_and_args = cd_args;   	// ou {"cd", NULL} si c'est un char **
 	cmd->path = NULL;               // cd est un builtin, pas de path binaire à chercher
 	cmd->fd_in = 0;                 // STDIN_FILENO (pas de redirection)
@@ -63,6 +63,110 @@ void exec_builtins(t_shell *shell, t_cmd *cmd)
 	else
 		ft_putstr_fd("minishell: command not found\n", 2);
 }
+
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+		free(tab[i++]);
+	free(tab);
+}
+
+char	*try_path(char *dir, char *cmd)
+{
+	char	*tmp;
+	char	*full;
+
+	tmp = ft_strjoin(dir, "/");
+	full = ft_strjoin(tmp, cmd);
+	free(tmp);
+	if (access(full, X_OK) == 0)
+		return (full);
+	free(full);
+	return (NULL);
+}
+
+char	*get_path(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*find_path(char *cmd, char **env)
+{
+	char	**dirs;
+	char	*path;
+	char	*result;
+	int		i;
+
+	if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+			return (cmd);
+		return (NULL);
+	}
+	path = get_path(env);
+	if (!path)
+		return (NULL);
+	dirs = ft_split(path, ':');
+	i = 0;
+	while (dirs[i])
+	{
+		result = try_path(dirs[i], cmd);
+		if (result)
+			return (free_tab(dirs), result);
+		i++;
+	}
+	i = 0;
+	return (free_tab(dirs), NULL);
+}
+
+void exec_external(t_cmd *cmd, char **env)
+{
+    char    *path;
+
+    path = find_path(cmd->cmd_and_args[0], env);
+    if (!path)
+    {
+        ft_putstr_fd(cmd->cmd_and_args[0], STDERR_FILENO);
+        ft_putstr_fd(": command not found\n", STDERR_FILENO);
+        exit(127);
+    }
+    execve(path, cmd->cmd_and_args, env);
+    perror("execve");
+    free(path);
+    exit(126);
+}
+
+void exec_single_external(t_cmd *cmd, char **env)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return ;
+	}
+	if (pid == 0)
+		exec_external(cmd, env);
+	/* waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		all->exit_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		all->exit_status = 128 + WTERMSIG(status); */
+}
 int	main(int ac, char **av, char **env)
 {
 	(void)ac;
@@ -72,9 +176,12 @@ int	main(int ac, char **av, char **env)
 
 	set_cmd(&cmd);
 	set_shell(&shell, &cmd, env);
-	printf("oldpwd: %s\n", getcwd(NULL, 0));
+
+	exec_single_external(shell.cmds, env);
+	
+	/* printf("oldpwd: %s\n", getcwd(NULL, 0));
 	exec_builtins(&shell, shell.cmds);
-	printf("newpwd: %s\n", getcwd(NULL, 0));
+	printf("newpwd: %s\n", getcwd(NULL, 0)); */
 		
 	return (0);
 }
