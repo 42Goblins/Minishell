7af8477827407d2f25737d7587db2b08dfe8885b
Dounia Geara <dougea71@gmail.com>
2026-08-17 18:19:13 +0200

    chore: #39 modified some stuff moved execbuiltin function prep working on exec_pipeline

---
+++ srcs/exec/exec_builtins.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_builtins.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgeara <dgeara@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 16:11:59 by dgeara            #+#    #+#             */
+/*   Updated: 2026/08/10 16:12:13 by dgeara           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void exec_builtins(t_shell *shell, t_cmd *cmd)
+{
+	if (ft_strncmp(cmd->cmd_and_args[0], "cd", 3) == 0)
+		*get_status() = exec_cd(shell, cmd->cmd_and_args);
+	else if (ft_strncmp(cmd->cmd_and_args[0], "echo", 5) == 0)
+		*get_status() = exec_echo(cmd->cmd_and_args);
+	else if (ft_strncmp(cmd->cmd_and_args[0], "env", 4) == 0)
+		*get_status() = exec_env(shell->env, cmd->cmd_and_args);
+	else if (ft_strncmp(cmd->cmd_and_args[0], "pwd", 4) == 0)
+		*get_status() = exec_pwd();
+	else if (ft_strncmp(cmd->cmd_and_args[0], "unset", 6) == 0)
+		*get_status() = exec_unset(&shell->env, cmd->cmd_and_args);
+	else if (ft_strncmp(cmd->cmd_and_args[0], "exit", 5) == 0)
+		*get_status() = exec_exit(cmd->cmd_and_args);
+	else if (ft_strncmp(cmd->cmd_and_args[0], "export", 7) == 0)
+		*get_status() = exec_export(&shell->env, cmd->cmd_and_args);
+}
